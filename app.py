6ce4d34368cbec315bdbd7a52f862ca760abb94d
luisa-bueno <61847137+luisa-bueno@users.noreply.github.com>
2023-03-16 19:55:16 -0300

    First commit of a WebSocket chat for image upload

---
+++ app.py
@@ -0,0 +1,93 @@
+import base64
+from curses import flash
+from flask import Flask, redirect, request, send_from_directory, url_for
+from flask.templating import render_template
+from fileinput import filename
+from flask_socketio import SocketIO, emit, send
+from werkzeug.datastructures import FileStorage
+from werkzeug.utils import secure_filename
+import os
+from io import BytesIO
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField, StringField
+
+app = Flask(__name__)
+io = SocketIO(app)
+
+
+app.config['SECRET_KEY'] = 'umasenhalegal'
+app.config['UPLOADED_PHOTOS_DEST'] = '/home/luisabueno/new_site/static/uploads'
+app.config['UPLOAD_FOLDER'] = '/home/luisabueno/new_site/static/uploads'
+
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+
+class UploadForm(FlaskForm):
+    photo = FileField(
+        validators=[
+            FileAllowed(photos, 'Apenas imagens são permitidas'),
+            FileRequired('Não pode deixar o campo de imagem vazio')
+        ],
+    )
+    submit = SubmitField('Upload')
+
+messages = []
+
+
+@app.route('/', methods=['POST','GET'])
+def upload_image_handler():
+    form = UploadForm()    
+    if form.validate_on_submit():
+        photo = form.photo.data
+        filename = photos.save(photo)
+        file_url = url_for('get_file_url', filename=filename)
+        messages.append(file_url)
+        print('\n\n')
+        print(messages)
+        print('\n\n')
+    else:
+        file_url = None
+        filename = None
+    return render_template('chat.html', form=form, file_url=file_url, filename=filename)
+
+
+#Abrir Imagem
+@app.route('/uploads/<filename>')
+def get_file_url(filename):
+    return send_from_directory(app.config['UPLOADED_PHOTOS_DEST'], filename)
+
+
+@io.on('sendMessage')
+def send_message_handler(data):
+    
+    # Extrair o objeto File do evento de submissão do formulário e convertê-lo
+    file_binary = data['file']
+    bytes_io = BytesIO(file_binary)
+
+    # Criar um objeto FileStorage a partir do objeto File
+    photo = FileStorage(stream=bytes_io, filename=data['filename'], content_type=data['content_type'])
+
+    # Salvar a foto e obter sua URL
+    filename = photos.save(photo)
+    file_url = url_for('get_file_url', filename=filename)
+
+    # Adicionar a URL à lista de mensagens
+    messages.append(file_url)
+
+    # Transmitir a URL para todos os clientes conectados
+    emit('getMessage', file_url, broadcast=True)
+
+
+
+@io.on('message')
+def message_handler(msg):
+
+    # envia mensagem para quem estiver ouvindo
+    send(messages)
+
+if __name__ == "__main__":
+    io.run(app, debug=True)
+
